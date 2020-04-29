bfdcbd8d18aa20a38e45a531b9782bcbc26b3a47
Cleilson <marcialwushu@gmail.com>
2020-04-29 03:13:55 -0300

    components header and footer create

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Footer = () =>  {
+    
+    return ( 
+        <div className="App">
+            MY FOOTER
+        </div>
+    );    
+    
+};
+
+export default Footer;
