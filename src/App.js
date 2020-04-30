fc7f37fd4188a7fa9424f354971a515a52af4920
Cleilson <marcialwushu@gmail.com>
2020-04-30 19:51:46 -0300

    Error: invariant falied: You should not use <Link> outsidea <Router>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import './App.css';
+
 
 function App() {
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,50 @@
 import React, {Component} from 'react';
+import './Header.css';
+import {Link} from 'react-router-dom';
 
 
 class Header extends Component {
     render() {
         return ( 
-            <div className="App">
-                MY HEADER
-            </div>
+          <nav className="navbar is-transparent">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="https://bulma.io">MyCompany</a>
+              <div className="navbar-burger burger" >
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              </div>
+
+            <div id="navbarExampleTransparentExample" className="navbar-menu" >
+              <div className="navbar-start">
+                <div className="navbar-item has-dropdown is-hoverable"></div>
+                </div>
+
+              <div className="navbar-end">
+
+              <Link to="/" className="nav-item r-item">Home</Link>
+              <Link to="/faq" className="nav-item r-item">Feature</Link>
+              <Link to="/faq" className="nav-item r-item">About</Link>
+              <Link to="/faq" className="nav-item r-item">FAQ</Link>
+
+                
+                <div className="navbar-item">
+                  <div className="field is-grouped">
+
+                    <p className="control">
+                      <a className="button is-primary is-outlined" href="https://bulma.io">
+                        <span className="icon">
+                          <i className="fas fa-download"></i>
+                          </span>
+                          <span>Join Now</span>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </nav>
         );
     }
     
