f47c939f6319a9a5c9f8da96c4f1a14b53838426
Cleilson <marcialwushu@gmail.com>
2020-05-20 20:04:55 -0300

    Updates

---
+++ precache-manifest.72f47b4656bf5ec0f462fe2ab6c6c7a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d4b004794d0340f53e937be78c9042",
+    "url": "/ReactjsByExample/index.html"
+  },
+  {
+    "revision": "98a876cd06661a2a1736",
+    "url": "/ReactjsByExample/static/css/main.6aa65f3a.chunk.css"
+  },
+  {
+    "revision": "8fd701e8f95a950dc30b",
+    "url": "/ReactjsByExample/static/js/2.12e2b358.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactjsByExample/static/js/2.12e2b358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a876cd06661a2a1736",
+    "url": "/ReactjsByExample/static/js/main.c9101467.chunk.js"
+  },
+  {
+    "revision": "22fbb3f3fa42d434f1d5",
+    "url": "/ReactjsByExample/static/js/runtime-main.4fa43dd3.js"
+  },
+  {
+    "revision": "d053e7cca6a956b08cd3f634d5798b5a",
+    "url": "/ReactjsByExample/static/media/clouds.d053e7cc.jpg"
+  }
+]);
